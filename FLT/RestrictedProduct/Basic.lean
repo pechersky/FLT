f67d4d6aac2cdf810bfbd5094a09e78d674bb0c7
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-16 15:26:43 +0000

    run into a problem

--- FLT/RestrictedProduct/Basic.lean
+++ FLT/RestrictedProduct/Basic.lean
@@ -21,7 +21,6 @@ example : RestrictedProduct (HeightOneSpectrum R) (fun v ↦ v.adicCompletion K)
 end finite_adele_example
 
 variable (I : Type*) (G : I → Type*)
--- {Ht : I → Type*} (H : ∀ i, Ht i) [∀ i, SetLike (Ht i) (G i)]
 
 section monoid
 
@@ -36,6 +35,65 @@ instance : Monoid (RestrictedProduct I G H) where
 
 end monoid
 
+section group_problem
+
+variable [∀ i, Group (G i)] (H : ∀ i, Subgroup (G i))
+
+--#synth Monoid (RestrictedProduct I G H) -- fails
+#synth Monoid (RestrictedProduct I G (fun i ↦ (H i).toSubmonoid)) -- works
+
+-- instance : Group (RestrictedProduct I G H) where
+--   inv x := ⟨fun i ↦ (x.val i)⁻¹, sorry⟩
+--   inv_mul_cancel := sorry
+
+-- ouch -- I might need help here.
+end group_problem
+
+-- I only need these things for: groups, rings,
+-- topological groups, topological rings
+section group
+
+variable [∀ i, Group (G i)] (H : ∀ i, Subgroup (G i))
+
+instance : Group (RestrictedProduct I G H) where
+  mul x y := ⟨fun i ↦ x.val i * y.val i, sorry⟩
+  mul_assoc := sorry
+  one := ⟨fun i ↦ 1, sorry⟩
+  one_mul := sorry
+  mul_one := sorry
+  inv x := ⟨fun i ↦ (x.val i)⁻¹, sorry⟩
+  inv_mul_cancel := sorry
+
+end group
+
+section ring
+
+variable [∀ i, Ring (G i)] (H : ∀ i, Subring (G i))
+
+instance : AddCommGroup (RestrictedProduct I G H) where
+  add x y := ⟨fun i ↦ x.val i + y.val i, sorry⟩
+  add_assoc x y z := sorry
+  zero := ⟨fun i ↦ 0, sorry⟩
+  zero_add x := sorry
+  add_zero x := sorry
+  add_comm x y := sorry
+  neg x := ⟨fun i ↦ -x.val i, sorry⟩
+  neg_add_cancel x := sorry
+
+instance : Ring (RestrictedProduct I G H) where
+  mul x y := ⟨fun i ↦ x.val i * y.val i, sorry⟩
+  mul_assoc := sorry
+  zero_mul x := sorry
+  mul_zero x := sorry
+  left_distrib x y z := sorry
+  right_distrib x y z := sorry
+  one := ⟨fun i ↦ 1, sorry⟩
+  one_mul := sorry
+  mul_one := sorry
+  zsmul := zsmulRec
+  -- errorI d
+
+end ring
 /-
 
 Group, CommRing, TopologicalSpace (careful with this one, it's not the subspace topology of ∏ᵢ Gᵢ,
